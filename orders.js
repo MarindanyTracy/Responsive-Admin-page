29adc1ac2b198a31658ee1927356da3f1fde2206
MarindanyTracy <marindanytracy0@gmail.com>
2022-09-14 12:25:14 +0300

    initial commit

---
+++ orders.js
@@ -0,0 +1,38 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '856821',
+        paymentStatus:'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'LAVENDAR KF102 Drone',
+        productNumber: '273821',
+        paymentStatus:'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Ruko f11 Drone',
+        productNumber: '128621',
+        paymentStatus:'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Drone with Camera rone',
+        productNumber: '497211',
+        paymentStatus:'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'GPS 4k Drone',
+        productNumber: '2273546',
+        paymentStatus:'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Lorenze Drone',
+        productNumber: '00457',
+        paymentStatus:'Paid',
+        shipping: 'Delivered'
+    }
+]
